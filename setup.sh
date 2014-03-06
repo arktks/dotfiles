ecc31fb1353cf59a17d809b88f33f2a1ad2aec17
arktks <go.go.monster65@gmail.com>
2014-03-06 14:43:03 +0900

    autojump導入

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ do
 done
 
 [ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-[ ! -d ~/.oh-my-zsh-powerline-theme ] && git clone git@github.com:jeremyFreeAgent/oh-my-zsh-powerline-theme.git ~/.oh-my-zsh-powerline-theme
+[ ! -d ~/.oh-my-zsh-powerline-theme ] && git clone https://github.com/jeremyFreeAgent/oh-my-zsh-powerline-theme.git ~/.oh-my-zsh-powerline-theme
 [ -d ~/.oh-my-zsh-powerline-theme ] && cd ~/.oh-my-zsh-powerline-theme && ./install_in_omz.sh
 [ ! -d ~/.vim/neobundle.vim ] && git clone git://github.com/Shougo/neobundle.vim ~/.vim/neobundle.vim
